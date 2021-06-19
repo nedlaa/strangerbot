73c98c9dd57e269ecccbe42ec4f792f69a53a12d
nedlaa <86164661+nedlaa@users.noreply.github.com>
2021-06-20 01:37:20 +0800

    edit text for sg bot

--- end.go
+++ end.go
@@ -22,13 +22,14 @@ func endConversationWorker(jobs <-chan EndConversationEvent) {
 			db.Exec("UPDATE users SET match_chat_id = NULL, available = 0, previous_match = ? WHERE chat_id = ?", u.MatchChatID, u.ChatID)
 			db.Exec("UPDATE users SET match_chat_id = NULL, available = 0, previous_match = ? WHERE chat_id = ?", u.ChatID, u.MatchChatID)
 
-			telegram.SendMessage(u.MatchChatID.Int64, "Your chat's ended! Start a new one! :D follow @unichatbotchannel for announcements/updates/ways to support us!", emptyOpts)
+			telegram.SendMessage(u.MatchChatID.Int64, "Your chat's ended! Start a new one! :D follow @singaporebotchannel for announcements/updates/ways to support us!  If the user you just chatted with did anything unpleasant or illegal, DO NOT start a new chat—contact the admin @aaldentnay immediately. Note that /report (REASON—do not leave blank)  should be sent while chat was still active; otherwise, contact admin to report (don't start new chat).", emptyOpts)
 			telegram.SendMessage(u.MatchChatID.Int64, "Type /start to get matched with a new partner", emptyOpts)
 		} else {
 			db.Exec("UPDATE users SET available = 0 WHERE chat_id = ?", u.ChatID)
 		}
 
-		telegram.SendMessage(u.ChatID, "Your conversation is over, I hope you enjoyed it :) follow @unichatbotchannel for announcements/updates/ways to support development!", emptyOpts)
+		telegram.SendMessage(u.ChatID, "Your conversation is over, I hope you enjoyed it :) follow @singaporebotchannel for announcements/updates/ways to support development! If the user you just chatted with did anything unpleasant or illegal, DO NOT start a new chat—contact the admin @aaldentnay immediately. Note that /report (REASON—do not leave blank)  should be sent while chat was still active; otherwise, contact admin to report (don't start new chat).", emptyOpts)
 		telegram.SendMessage(u.ChatID, "Type /start to get matched with a new partner", emptyOpts)
 	}
 }
+
