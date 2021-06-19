9a492f88011b688dd3cbae5054165dc6ad9bcd1c
nedlaa <86164661+nedlaa@users.noreply.github.com>
2021-06-20 01:35:08 +0800

    text changed for sg bot

--- commands.go
+++ commands.go
@@ -54,7 +54,7 @@ func commandStart(u User, m *tgbotapi.Message) bool {
 
 	db.Exec("UPDATE users SET available = 1 WHERE chat_id = ?", u.ChatID)
 
-	telegram.SendMessage(u.ChatID, "Looking for another student to match you with... Hold on! (This may take a while! Keep your notifications on!) **NOTE: If you send anything illegal here, your data will be handed over to the police. Your User ID is anonymous only until you break the rules. A police report for harassment/defamation will be filed if you pass off another user's contact as if it is yours.** To report a user, enter **/report (followed by a reason; don't leave blank)** into the chat. If chat with a user you want to report has already ended, then **do not** start a new chat—immediately contact the admin @aaldentnay . **Also, a note to some guys here—pls stop being thirsty on here because that scares new users away; I'm taking a huge leapt of faith when I set up a platform like this. Those reported will be PERMANENTLY banned.** Misuse of /report , if not accidental, can also result in ban.", emptyOpts)
+	telegram.SendMessage(u.ChatID, "Looking for a cool person to match you with... Hold on! (This may take a while! Keep your notifications on!) **NOTE: If you send anything illegal here, your data will be handed over to the police. Your User ID is anonymous only until you break the rules. A police report for harassment/defamation will be filed if you pass off another user's contact as if it is yours.** To report a user, enter **/report (followed by a reason; don't leave blank)** into the chat. If chat with a user you want to report has already ended, then **do not** start a new chat—immediately contact the admin @aaldentnay . The 'friends/wholesome talk' segment must be purely for that only. Those reported will be PERMANENTLY banned.** Misuse of /report , if not accidental, can also result in ban. Do not meet up with anonymous strangers. Follow @singaporebotchannel for updates and more cool stuff!", emptyOpts)
 	startJobs <- u.ChatID
 
 	return true
@@ -194,15 +194,18 @@ func commandHelp(u User, m *tgbotapi.Message) bool {
 	}
 
 	telegram.SendMessage(m.Chat.ID, `Help:
+Use /setup to configure your profile first.
 
 Use /start to start looking for a conversational partner, once you're matched you can use /end to end the conversation.
 
 Use /report to report a user, use it as follows:
 /report <reason>
 
+Stating a reason is required. If you fail to report because chat is over, then do not start a new chat and contact @aaldentnay immediately.
+
 Use /nopics to disable receiving photos, and /nopics if you want to enable it again.
 
-HEAD OVER to @unichatbotchannel for rules, updates, announcements or info on how you can support the bot!
+HEAD OVER to @singaporebotchannel for rules, updates, announcements or info on how you can support the bot!
 
 Sending images and videos are a beta functionality, but appear to be working fine.
 
